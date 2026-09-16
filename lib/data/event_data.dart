abdb888520ec97113d0adb769f32e5a1f44ff8aa
JoelNarvaez <joelnarmar20@gmail.com>
2026-09-16 15:01:56 -0600

    feature: se hicieron un total de 22 eventos

--- lib/data/event_data.dart
+++ lib/data/event_data.dart
@@ -93,4 +93,144 @@ const List<Map<String, dynamic>> eventos = [
     'imagen':
         'https://images.unsplash.com/photo-1501281668745-f7f57925c3b4?w=900',
   },
+  {
+    'titulo': 'Conferencia de Liderazgo Universitario',
+    'categoria': 'Académicos',
+    'fecha': '7 de octubre',
+    'hora': '10:00',
+    'lugar': 'Auditorio Central',
+    'cupo': 150,
+    'imagen':
+        'https://images.unsplash.com/photo-1544531586-fde5298cdd40?w=900',
+  },
+  {
+    'titulo': 'Torneo de Baloncesto Interfacultades',
+    'categoria': 'Deportivos',
+    'fecha': '8 de octubre',
+    'hora': '17:00',
+    'lugar': 'Gimnasio Universitario',
+    'cupo': 100,
+    'imagen':
+        'https://images.unsplash.com/photo-1579487685737-e435a87b2518?w=900',
+  },
+  {
+    'titulo': 'Meetup de Desarrollo de Software',
+    'categoria': 'Tecnología',
+    'fecha': '9 de octubre',
+    'hora': '18:00',
+    'lugar': 'Centro de Innovación',
+    'cupo': 60,
+    'imagen':
+        'https://images.unsplash.com/photo-1631350397792-8e0c2de5b637?w=900',
+  },
+  {
+    'titulo': 'Noche de Danza Folclórica',
+    'categoria': 'Culturales',
+    'fecha': '10 de octubre',
+    'hora': '19:00',
+    'lugar': 'Teatro Universitario',
+    'cupo': 180,
+    'imagen':
+        'https://images.unsplash.com/photo-1463592177119-bab2a00f3ccb?w=900',
+  },
+  {
+    'titulo': 'Taller de Robótica Educativa',
+    'categoria': 'Talleres',
+    'fecha': '11 de octubre',
+    'hora': '14:00',
+    'lugar': 'Laboratorio de Robótica',
+    'cupo': 30,
+    'imagen':
+        'https://images.unsplash.com/photo-1655393001768-d946c97d6fd1?w=900',
+  },
+  {
+    'titulo': 'Simposio de Investigación Científica',
+    'categoria': 'Académicos',
+    'fecha': '13 de octubre',
+    'hora': '09:00',
+    'lugar': 'Auditorio Central',
+    'cupo': 120,
+    'imagen':
+        'https://images.unsplash.com/photo-1492538368677-f6e0afe31dcc?w=900',
+  },
+  {
+    'titulo': 'Copa Universitaria de Voleibol',
+    'categoria': 'Deportivos',
+    'fecha': '14 de octubre',
+    'hora': '16:00',
+    'lugar': 'Cancha Universitaria',
+    'cupo': 70,
+    'imagen':
+        'https://images.unsplash.com/photo-1547347298-4074fc3086f0?w=900',
+  },
+  {
+    'titulo': 'Bootcamp de Programación',
+    'categoria': 'Tecnología',
+    'fecha': '15 de octubre',
+    'hora': '09:00',
+    'lugar': 'Laboratorio de Cómputo',
+    'cupo': 45,
+    'imagen':
+        'https://images.unsplash.com/photo-1756273343749-63f7d6ea0cda?w=900',
+  },
+  {
+    'titulo': 'Festival de Tradiciones Mexicanas',
+    'categoria': 'Culturales',
+    'fecha': '17 de octubre',
+    'hora': '17:00',
+    'lugar': 'Plaza Principal',
+    'cupo': 250,
+    'imagen':
+        'https://images.unsplash.com/photo-1759738102510-ec524f666274?w=900',
+  },
+  {
+    'titulo': 'Taller de Automatización y Electrónica',
+    'categoria': 'Talleres',
+    'fecha': '18 de octubre',
+    'hora': '15:00',
+    'lugar': 'Edificio 5',
+    'cupo': 28,
+    'imagen':
+        'https://images.unsplash.com/photo-1637002722490-5f8ceed9774c?w=900',
+  },
+  {
+    'titulo': 'Coloquio de Ciencias Sociales',
+    'categoria': 'Académicos',
+    'fecha': '20 de octubre',
+    'hora': '11:00',
+    'lugar': 'Sala Audiovisual',
+    'cupo': 80,
+    'imagen':
+        'https://images.unsplash.com/photo-1655337690778-694dda7f4454?w=900',
+  },
+  {
+    'titulo': 'Clásico Deportivo UAA',
+    'categoria': 'Deportivos',
+    'fecha': '21 de octubre',
+    'hora': '18:00',
+    'lugar': 'Gimnasio Universitario',
+    'cupo': 200,
+    'imagen':
+        'https://images.unsplash.com/photo-1581390561595-2544b8d26538?w=900',
+  },
+  {
+    'titulo': 'Foro de Innovación Tecnológica',
+    'categoria': 'Tecnología',
+    'fecha': '23 de octubre',
+    'hora': '12:00',
+    'lugar': 'Centro de Innovación',
+    'cupo': 90,
+    'imagen':
+        'https://images.unsplash.com/photo-1632910121591-29e2484c0259?w=900',
+  },
+  {
+    'titulo': 'Seminario de Metodología de la Investigación',
+    'categoria': 'Académicos',
+    'fecha': '24 de octubre',
+    'hora': '10:00',
+    'lugar': 'Auditorio Central',
+    'cupo': 100,
+    'imagen':
+        'https://images.unsplash.com/photo-1758270704262-ecc82b23dc37?w=900',
+  },
 ];
